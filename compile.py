7f0561af910ceda39f612ddc33ee4921f95de6a1
S. Co1 <sco1.git@gmail.com>
2026-01-04 12:11:03 -0500

    Bump vendored files for CP10.x

--- compile.py
+++ compile.py
@@ -7,9 +7,9 @@ SOURCE_DIR = Path("./skyportal")
 DEST_DIR = Path("./dist/lib/skyportal")
 
 COMPILERS = {
-    "Windows": Path("./mpy-cross/mpy-cross-windows-9.2.2.static.exe"),
-    "Linux": Path("./mpy-cross/mpy-cross-linux-amd64-9.2.2.static"),
-    "Darwin": Path("./mpy-cross/mpy-cross-macos-9.2.2-arm64"),
+    "Windows": Path("./mpy-cross/mpy-cross-windows-10.0.3.static.exe"),
+    "Linux": Path("./mpy-cross/mpy-cross-linux-amd64-10.0.3.static"),
+    "Darwin": Path("./mpy-cross/mpy-cross-macos-10.0.3-arm64"),
 }
 
 
